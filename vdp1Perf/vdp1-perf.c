fb034fc375cc3e1636fa8e163f38a5fd4260be15
Francois CARON <francois.caron@renault.com>
2022-01-05 23:54:44 +0100

    V2

--- vdp1Perf/vdp1-perf.c
+++ vdp1Perf/vdp1-perf.c
@@ -33,18 +33,22 @@
 #define PRIMITIVE_DRAW_MODE_GOURAUD_HALF_TRANS    (7)
 #define PRIMITIVE_DRAW_MODE_COUNT                 (8)
 
-#define PRIMITIVE_WIDTH           32
-#define PRIMITIVE_HEIGHT          32
+#define PRIMITIVE_WIDTH           64
+#define PRIMITIVE_HEIGHT          64
 #define PRIMITIVE_HALF_WIDTH      (PRIMITIVE_WIDTH / 2)
 #define PRIMITIVE_HALF_HEIGHT     (PRIMITIVE_HEIGHT / 2)
 
 #define PRIMITIVE_COLOR           COLOR_RGB1555(1, 31, 0, 31)
 
+#define MS_VALUE (CPU_FRT_PAL_320_8_COUNT_1MS)
+
+#define NB_CMD (1<<8)
+
 #define ORDER_SYSTEM_CLIP_COORDS_INDEX  0
 #define ORDER_LOCAL_COORDS_INDEX        1
 #define ORDER_POLYGON_INDEX             2
-#define ORDER_DRAW_END_INDEX            3
-#define ORDER_COUNT                     4
+#define ORDER_DRAW_END_INDEX            (NB_CMD+ORDER_POLYGON_INDEX)
+#define ORDER_COUNT                     (ORDER_DRAW_END_INDEX+1)
 
 static vdp1_cmdt_list_t *_cmdt_list = NULL;
 static vdp1_vram_partitions_t _vdp1_vram_partitions;
@@ -90,57 +94,79 @@ static vdp1_cmdt_draw_mode_t _primitive_draw_modes[] = {
         }
 };
 
-static char *_primitive_type_strings[] = {
-        "POLYLINE",
-        "POLYGON"
-};
-
-static char *_primitive_draw_mode_strings[] = {
-        "NORMAL",
-        "MESH",
-        "SHADOW",
-        "HALF-LUMINANCE",
-        "REPLACE/HALF-TRANSPARENT",
-        "GOURAUD",
-        "GOURAUD+HALF-LUMINANCE",
-        "GOURAUD+HALF-TRANSPARENT"
-};
+// static char *_primitive_type_strings[] = {
+//         "POLYLINE",
+//         "POLYGON"
+// };
+//
+// static char *_primitive_draw_mode_strings[] = {
+//         "NORMAL",
+//         "MESH",
+//         "SHADOW",
+//         "HALF-LUMINANCE",
+//         "REPLACE/HALF-TRANSPARENT",
+//         "GOURAUD",
+//         "GOURAUD+HALF-LUMINANCE",
+//         "GOURAUD+HALF-TRANSPARENT"
+// };
 
 static void _cmdt_list_init(void);
 static void _primitive_init(void);
 
-static void _vblank_out_handler(void *);
+static volatile uint16_t _frt_ovf_count = 0;
+
+static void
+_frt_ovi_handler(void)
+{
+        _frt_ovf_count++;
+}
+
+static void
+_timing_start(void)
+{
+        cpu_frt_count_set(0);
+        _frt_ovf_count = 0;
+}
+
+static fix16_t
+_timing_get(void)
+{
+        const uint32_t ticks_rem = cpu_frt_count_get();
+
+        const uint32_t overflow_count =
+            ((65536 / MS_VALUE) * _frt_ovf_count);
+
+        const uint32_t total_ticks =
+            (fix16_int32_from(overflow_count) + (fix16_int32_from(ticks_rem) / MS_VALUE));
+
+        return total_ticks;
+}
 
 void
 main(void)
 {
-        dbgio_dev_default_init(DBGIO_DEV_VDP2_SIMPLE);
+        dbgio_dev_default_init(DBGIO_DEV_VDP2_ASYNC);
         dbgio_dev_font_load();
-        dbgio_dev_font_load_wait();
 
         _cmdt_list_init();
         _primitive_init();
-
-        vdp1_cmdt_t *cmdt_polygon;
-        cmdt_polygon = &_cmdt_list->cmdts[ORDER_POLYGON_INDEX];
-
-        vdp1_cmdt_polygon_set(cmdt_polygon);
-
-        dbgio_printf("[H[2J"
-                     "     type: %s\n"
-                     "draw_mode: %s\n",
-                     _primitive_type_strings[_primitive.type],
-                     _primitive_draw_mode_strings[_primitive.draw_mode]);
-
-        dbgio_flush();
-
-        vdp1_cmdt_param_draw_mode_set(cmdt_polygon,
-            _primitive_draw_modes[_primitive.draw_mode]);
-        vdp1_cmdt_param_vertices_set(cmdt_polygon, &_primitive.points[0]);
-
-        vdp1_sync_cmdt_list_put(_cmdt_list, 0, NULL, NULL);
-        vdp_sync();
-        while (true) {}
+        cpu_frt_init(CPU_FRT_CLOCK_DIV_8);
+        cpu_frt_ovi_set(_frt_ovi_handler);
+        while(true) {
+          _timing_start();
+          vdp1_sync_cmdt_list_put(_cmdt_list, 0);
+          fix16_t frt_count = _timing_get();
+          vdp1_sync_render();
+          vdp1_sync();
+          while(vdp1_cmdt_current_get() != ORDER_DRAW_END_INDEX) {}
+          frt_count = _timing_get() - frt_count;
+          vdp1_sync_wait();
+          dbgio_puts("[1;1H[2J");
+          dbgio_printf("\n""0x%x %x Done\n", NB_CMD, frt_count);
+          dbgio_flush();
+          vdp2_sync();
+          vdp2_sync_wait();
+        }
 }
 
 void
@@ -159,13 +185,12 @@ user_init(void)
 
         vdp1_env_set(&env);
 
-        vdp_sync_vblank_out_set(_vblank_out_handler);
-
         cpu_intc_mask_set(0);
 
         vdp2_tvmd_display_set();
 
         vdp1_vram_partitions_get(&_vdp1_vram_partitions);
+
 }
 
 static void
@@ -200,23 +225,6 @@ _primitive_init(void)
             INT16_VEC2_INITIALIZER((SCREEN_WIDTH / 2) - PRIMITIVE_HALF_WIDTH - 1,
                                       (SCREEN_HEIGHT / 2) - PRIMITIVE_HALF_HEIGHT - 1);
 
-        _primitive.type = PRIMITIVE_TYPE_POLYLINE;
-        _primitive.draw_mode = PRIMITIVE_DRAW_MODE_NORMAL;
-
-        _primitive.color = PRIMITIVE_COLOR;
-
-        _primitive.points[0].x = 0;
-        _primitive.points[0].y = PRIMITIVE_HEIGHT - 1;
-
-        _primitive.points[1].x = PRIMITIVE_WIDTH - 1;
-        _primitive.points[1].y = PRIMITIVE_HEIGHT - 1;
-
-        _primitive.points[2].x = PRIMITIVE_WIDTH - 1;
-        _primitive.points[2].y = 0;
-
-        _primitive.points[3].x = 0;
-        _primitive.points[3].y = 0;
-
         vdp1_cmdt_t *cmdt_local_coords;
         cmdt_local_coords = &_cmdt_list->cmdts[ORDER_LOCAL_COORDS_INDEX];
 
@@ -224,8 +232,6 @@ _primitive_init(void)
         vdp1_cmdt_param_vertex_set(cmdt_local_coords, CMDT_VTX_LOCAL_COORD,
             &local_coord_center);
 
-        vdp1_cmdt_t *cmdt_polygon;
-        cmdt_polygon = &_cmdt_list->cmdts[ORDER_POLYGON_INDEX];
 
         vdp1_gouraud_table_t *gouraud_base;
         gouraud_base = _vdp1_vram_partitions.gouraud_base;
@@ -235,17 +241,31 @@ _primitive_init(void)
         gouraud_base->colors[2] = COLOR_RGB1555(1,  0,  0, 31);
         gouraud_base->colors[3] = COLOR_RGB1555(1, 31, 31, 31);
 
-        vdp1_cmdt_polyline_set(cmdt_polygon);
-        vdp1_cmdt_param_color_set(cmdt_polygon, _primitive.color);
-        vdp1_cmdt_param_draw_mode_set(cmdt_polygon,
-            _primitive_draw_modes[_primitive.draw_mode]);
-        vdp1_cmdt_param_vertices_set(cmdt_polygon, &_primitive.points[0]);
+        for (int i = 0; i<NB_CMD; i++){
+          vdp1_cmdt_t *cmdt_polygon;
+          cmdt_polygon = &_cmdt_list->cmdts[ORDER_POLYGON_INDEX+i];
 
-        vdp1_cmdt_param_gouraud_base_set(cmdt_polygon, (uint32_t)gouraud_base);
-}
+          _primitive.type = PRIMITIVE_TYPE_POLYLINE;
+          _primitive.draw_mode = PRIMITIVE_DRAW_MODE_NORMAL;
 
-static void
-_vblank_out_handler(void *work __unused)
-{
-        smpc_peripheral_intback_issue();
+          _primitive.color = COLOR_RGB1555(1, (31+i)%32, (0+i)%32, (31+i)%32);
+
+          _primitive.points[0].x = (i - NB_CMD/2);
+          _primitive.points[0].y = PRIMITIVE_HEIGHT - 1 + (i - NB_CMD/2);
+
+          _primitive.points[1].x = PRIMITIVE_WIDTH - 1 + (i - NB_CMD/2);
+          _primitive.points[1].y = PRIMITIVE_HEIGHT - 1 + (i - NB_CMD/2);
+
+          _primitive.points[2].x = PRIMITIVE_WIDTH - 1 + (i - NB_CMD/2);
+          _primitive.points[2].y = (i - NB_CMD/2);
+
+          _primitive.points[3].x = (i - NB_CMD/2);
+          _primitive.points[3].y = (i - NB_CMD/2);
+
+          vdp1_cmdt_param_color_set(cmdt_polygon, _primitive.color);
+          vdp1_cmdt_param_draw_mode_set(cmdt_polygon, _primitive_draw_modes[_primitive.draw_mode]);
+          vdp1_cmdt_param_vertices_set(cmdt_polygon, &_primitive.points[0]);
+          vdp1_cmdt_param_gouraud_base_set(cmdt_polygon, (uint32_t)gouraud_base);
+          vdp1_cmdt_polygon_set(cmdt_polygon);
+        }
 }
